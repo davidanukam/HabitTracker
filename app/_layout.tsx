026ab8dab2c5b2630c7f03728923cb5aa1a0c434
Duzzenn <davidanukam72@gmail.com>
2025-06-13 01:19:43 -0400

    chore: update package dependencies and restructure layout with authentication guard

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,18 +1,38 @@
-import { Stack, Tabs } from "expo-router";
-import FontAwesome5 from "@expo/vector-icons/FontAwesome5";
+import { AuthProvider } from "@/lib/auth-context";
+import { Stack, useRouter } from "expo-router";
+import React, { useEffect, useState } from "react";
+
+function RouteGuard({ children }: Readonly<{ children: React.ReactNode }>) {
+  const router = useRouter();
+  const isAuth = false;
+  const [hasMounted, setHasMounted] = useState(false);
+
+  useEffect(() => {
+    setHasMounted(true);
+  }, []);
+
+  useEffect(() => {
+    if (hasMounted && !isAuth) {
+      router.replace("/auth");
+    }
+  }, [hasMounted, isAuth, router]);
+
+  if (!hasMounted) {
+    return null;
+  }
+
+  return <>{children}</>;
+}
 
 export default function RootLayout() {
   return (
-    <Tabs screenOptions={{ tabBarActiveTintColor: "blue" }}>
-      <Tabs.Screen
-        name="index"
-        options={{
-          title: "Home",
-          tabBarIcon: ({ color }) => (
-            <FontAwesome5 name="home" size={24} color={color} />
-          ),
-        }}
-      />
-    </Tabs>
+    <AuthProvider>
+        <RouteGuard>
+          <Stack>
+            <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+            <Stack.Screen name="auth" options={{ headerShown: false }} />
+          </Stack>
+        </RouteGuard>
+    </AuthProvider>
   );
 }
